cc312d8b64f22aee5650a69cae0e45a4825ca552
Ангархаев Аюр <ayur_12@bk.ru>
2023-03-06 00:55:27 +0800

    1

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MinApp;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMin()
+        {
+            int min;
+            min = Program.Min(7, 2, 5);
+            Assert.AreEqual(2, min);
+        }
+    }
+}
